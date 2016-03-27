866a7f23aad1917f9559ddcfe798d62c292e874a
radi81 <radi81>
2016-03-27 22:32:40 +0300

    Homework 1

---
+++ Homework1/UnityBasicsHomework/Assets/Scripts/FollowScriptCube.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowScriptCube : MonoBehaviour 
+{ 
+	public Transform cylinder;
+	public float followSpeed;
+	public float minDistance;
+
+	// Update is called once per frame
+	void LateUpdate () 
+	{
+		if (cylinder != null) 
+		{
+			this.transform.LookAt (cylinder);
+
+			if (Vector3.Distance (this.transform.position, cylinder.position) > minDistance)
+			{
+				this.transform.Translate (0f, 0f, followSpeed * Time.deltaTime);
+			}
+		}	
+	}
+}
---
+++ Homework1/UnityBasicsHomework/Assets/Scripts/FollowScriptSphere.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowScriptSphere : MonoBehaviour 
+{
+	public Transform cube;
+	public float followSpeed;
+	public float minDistance;
+
+	// Update is called once per frame
+	void LateUpdate () 
+	{
+		if (cube != null) 
+		{
+			this.transform.LookAt (cube);
+
+			if (Vector3.Distance (this.transform.position, cube.position) > minDistance)
+			{
+				this.transform.Translate (0f, 0f, followSpeed * Time.deltaTime);
+			}
+		}	
+	}
+}
